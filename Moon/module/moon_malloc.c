e86af38a8611a087467d733455bc6d4100417e09
lengkristy <lengkristy@163.com>
2018-05-01 22:15:16 +0800

    Translate Chinese comments into English annotations.Translate Chinese comments into English annotations.

--- Moon/module/moon_malloc.c
+++ Moon/module/moon_malloc.c
@@ -6,9 +6,8 @@ extern "C" {
 
 void* moon_malloc(unsigned int num_bytes)/*allocate memory*/
 {
-	/*以后需要增加内存池的管理*/
 	void* pMemory = malloc(num_bytes);
-	memset(pMemory,0,num_bytes);//初始化为0
+	memset(pMemory,0,num_bytes);
 	return pMemory;
 }
 
